b5a26d80164ce687ae8a2b00984f94a588f35ece
Andrey Bronnikov <andreyb@digitalpersona.com>
2019-09-20 13:44:28 -0700

    * fixed tests

--- src/tokens/questions/enroll.spec.ts
+++ src/tokens/questions/enroll.spec.ts
@@ -1,29 +1,36 @@
-import { Env } from '../../test'
-import { SecurityQuestionsEnroll } from '.'
 import { User, JSONWebToken, Question, Answer } from '@digitalpersona/core';
 import { AuthService, EnrollService } from '@digitalpersona/services';
 import { PasswordAuth } from '@digitalpersona/authentication';
 
-describe("Questions Token: ", ()=>
+import { Env } from '../../test';
+import { EnrollmentContext } from '../../enrollmentContext';
+import { SecurityQuestionsEnroll } from '.';
+
+describe("Questions Token: ", () =>
 {
     let api: SecurityQuestionsEnroll;
     let officer: JSONWebToken;
+    let context: EnrollmentContext;
 
     const user: User = new User("alpha\\administrator");
+    const service = new EnrollService(Env.EnrollServerEndpoint);
 
-    beforeEach(async ()=>{
-        api = new SecurityQuestionsEnroll(
-            new EnrollService(Env.EnrollServerEndpoint)
-        );
-        const passwordAuth = new PasswordAuth(new AuthService(Env.AuthServerEndpoint));
-        officer = await passwordAuth.authenticate(user, "aaaAAA123");
-    })
+    beforeEach(async () => {
+        if (Env.Integration) {
+            const passwordAuth = new PasswordAuth(new AuthService(Env.AuthServerEndpoint));
+            officer = await passwordAuth.authenticate(user, "aaaAAA123");
+        }
+        context = new EnrollmentContext(service, user, officer);
+        api = new SecurityQuestionsEnroll(context);
+    });
 
     it("must enroll", async () => {
         if (!Env.Integration) return;
-        const questions = [1, 2, 3].map(n => new Question(n, 9, 1, 1033));
-        const answers = [2,3,1].map(n => new Answer(n, "aaaaaa"));
+        const answers = [1, 2, 3].map(n => ({
+            question: new Question(n, 9, 1, 1033),
+            answer: new Answer(n, 'aaaaaa'),
+        }));
 
-        await expectAsync(api.enroll(officer, questions, answers)).toBeResolved();
-    })
-})
+        await expectAsync(api.enroll(answers)).toBeResolved();
+    });
+});
